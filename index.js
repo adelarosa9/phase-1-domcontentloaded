e413f2dbda29ac10c263b61cb7ef2f40ba94b039
alejandro delarosa <a.delarosa9@yahoo.com>
2024-01-07 11:38:29 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    // Target the paragraph element with id="text"
+    const textParagraph = document.getElementById("text");
+  
+    // Replace the text with "This is really cool!"
+    if (textParagraph) {
+      textParagraph.textContent = "This is really cool!";
+    }
+  });
+  
